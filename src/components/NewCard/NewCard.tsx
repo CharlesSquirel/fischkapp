32b424b06ef45bd1f49a51674542fd2638b6c37d
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-09-20 13:39:58 +0200

    Fixing bus with one card at the same time

--- src/components/NewCard/NewCard.tsx
+++ src/components/NewCard/NewCard.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles/NewCard.module.scss";
 import FrontNewCard from "./FrontNewCard";
 import BackNewCard from "./BackNewCard";
 
 const NewCard: React.FC = () => {
+  const [isCardReversed, setIsCardReversed] = useState(false)
   return (
     <div className={styles.cardContainerMain}>
       <FrontNewCard />
-      <BackNewCard />
+      {isCardReversed && <BackNewCard />}
     </div>
   );
 };
