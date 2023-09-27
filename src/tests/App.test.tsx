7d6d74393376d416da7eb928474b26785b49d8df
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-09-27 17:23:02 +0200

    Fixing gitignore

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,12 +1,11 @@
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import NewCard from "../components/NewCard/NewCard";
-import App, { Context } from "../App";
+import { Context } from "../App";
 import { ContextProps, initialCardText } from "../components/services/types/types";
 import { AppHeader } from "../components/Header/AppHeader";
 import { AppLayout } from "../components/AppLayout";
 import CardList from "../components/CardList/CardList";
-import BackNewCard from "../components/NewCard/BackNewCard";
 
 const contextTestValue: ContextProps = {
   isNewCardshowed: false,
@@ -37,26 +36,22 @@ it("render App", () => {
   expect(newCardComponent).toBeInTheDocument();
   const FrontCardValue = screen.getByRole("textbox") as HTMLTextAreaElement;
   const nextBtn = screen.getByText("Next");
-  const {newCardTexts} = contextTestValue;
+  const { newCardTexts } = contextTestValue;
   if (FrontCardValue.value.length <= 0) {
-    expect(nextBtn).toHaveAttribute("disabled", "")
-  }
-  else {
-    expect(nextBtn).toHaveAttribute("disabled", newCardTexts.front)
-    fireEvent.click(nextBtn)
-    const backNewCardComponent = screen.getByTestId("back-new-card")
-    const frontNewCardComponent = screen.getByTestId("front-new-card")
-    expect(backNewCardComponent).toBeInTheDocument()
-    expect(frontNewCardComponent).not.toBeInTheDocument()
-    const saveBtn = screen.getByText("Save")
+    expect(nextBtn).toHaveAttribute("disabled", "");
+  } else {
+    expect(nextBtn).toHaveAttribute("disabled", newCardTexts.front);
+    fireEvent.click(nextBtn);
+    const backNewCardComponent = screen.getByTestId("back-new-card");
+    const frontNewCardComponent = screen.getByTestId("front-new-card");
+    expect(backNewCardComponent).toBeInTheDocument();
+    expect(frontNewCardComponent).not.toBeInTheDocument();
+    const saveBtn = screen.getByText("Save");
     const BackCardValue = screen.getByRole("textbox") as HTMLTextAreaElement;
     if (BackCardValue.value.length <= 0) {
-      expect(saveBtn).toHaveAttribute("disabled", "")
-    }
-    else {
-      expect(saveBtn).toHaveAttribute("disabled", newCardTexts.back)
+      expect(saveBtn).toHaveAttribute("disabled", "");
+    } else {
+      expect(saveBtn).toHaveAttribute("disabled", newCardTexts.back);
     }
   }
-
-
 });
